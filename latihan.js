998e09e1b524c3748e109a39f2d45f06c14f1bb3
Rada Pranata <pranatarada@gmail.com>
2026-09-17 15:18:44 +0700

    latihan variabel tipe data dan operator

---
+++ latihan.js
@@ -0,0 +1,22 @@
+const namaSaya = "Rada Pranata";
+const umurSaya = 25;
+const kotaSaya = "Batam";
+
+console.log(namaSaya);
+console.log(umurSaya);
+console.log(kotaSaya);
+
+let score = 0;
+console.log(score); //cetak nilai awal, harusnya 0
+
+score = 100;
+console.log(score); //cetak nilai score setelah diubah, harusnya 100
+
+console.log(10 % 3);
+
+const punyaKTp = true;
+const umurCukup = false;
+
+console.log(punyaKTp && umurCukup); // AND - keduanya harus true
+console.log(punyaKTp || umurCukup); // OR - salah satu harus true
+console.log(!punyaKTp); // NOT - membalik nilai boolean
