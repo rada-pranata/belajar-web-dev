90ee21ef8716bb7089ebc43bb27ae05448c76973
Rada Pranata <pranatarada@gmail.com>
2026-09-17 15:47:58 +0700

    latihan control flow if else dan for loop

---
+++ latihan2.js
@@ -0,0 +1,17 @@
+const umur = 20;
+
+if (umur >= 17) {
+  console.log("Anda sudah dewasa");
+} else {
+  console.log("Anda masih di bawah umur");
+}
+
+for (let i = 1; i <= 10; i++) {
+  console.log(i);
+}
+
+for (let i = 1; i <= 10; i++) {
+  if (i % 2 === 0) {
+    console.log(i);
+  }
+}
